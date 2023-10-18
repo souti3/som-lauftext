3f276810393e3f8b7ee6e8f58a011f53fb014685
Marcel Soltermann <marcel.soltermann@students.fhnw.ch>
2023-10-18 23:34:15 +0200

    [CHG] added all the pixels with id

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const numOfPixels = 30;
+const numOfPixelColumns = 30;
+const numOfPixelRows = 10;
 
 // Select the container div
 const container = document.getElementById("textboarder");
 
 // Loop to create and append elements
-for (let i = 0; i < numOfPixels; i++) {
-    const divFirstLine = document.createElement("div");
-    divFirstLine.className = "firstLine";
-    container.appendChild(divFirstLine);
-}
-
-for (let j = 0; j < numOfPixels; j++) {
-    const divSecondLine = document.createElement("div");
-    divSecondLine.className = "secondLine";
-    container.appendChild(divSecondLine);
+for (let i = 0; i < numOfPixelColumns; i++) {
+    for (let j = 0; j < numOfPixelRows; j++) {
+        const divFirstLine = document.createElement("div");
+        divFirstLine.className = "pixelElement";
+        divFirstLine.id="" + i + j;
+        container.appendChild(divFirstLine);
+    }
+    
 }
